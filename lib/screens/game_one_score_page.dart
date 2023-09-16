d4a5db0b4276d493b06549c97cbe573e71e46439
Emre Kaya <emrekaya75@outlook.com>
2023-09-16 18:12:43 +0300

    app ready v1.0.1

--- lib/screens/game_one_score_page.dart
+++ lib/screens/game_one_score_page.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:futquiz/model/user_model.dart';
 
 class GameOneScorePage extends StatelessWidget {
